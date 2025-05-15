ac63c3c560d56e2a7c60f292de91f0b4804bf842
nicolas.bertelsen <stilas.bertelsen@gmail.com>
2025-05-15 14:10:42 +0200

    disabled tests temporarily

--- src/test/java/dia/ismd/IsmdBackendApplicationTests.java
+++ src/test/java/dia/ismd/IsmdBackendApplicationTests.java
@@ -1,5 +1,6 @@
 package dia.ismd;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
@@ -9,10 +10,13 @@ import org.springframework.modulith.test.ApplicationModuleTest;
 @ApplicationModuleTest
 class IsmdBackendApplicationTests {
 
+    @Disabled("Temporarily disabled for CI workflow testing")
     @Test
     void contextLoads() {
     }
 
+
+    @Disabled("Temporarily disabled for CI workflow testing")
     @Test
     void modulesTest(){
         ApplicationModules.of(IsmdBackendApplication.class).verify();
